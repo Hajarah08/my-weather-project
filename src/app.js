3ca34f5934add0051dc48237ab810539e5ba9569
Hajarah08 <koikiadetutu@gmail.com>
2022-03-31 17:02:50 +0000

    created app.js and added it to src folder

---
+++ src/app.js
@@ -0,0 +1,43 @@
+let now = new Date();
+let days = ["Sun", "Mon", "Wed", "Thurs", "Fri", "Sat"];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let date = now.getDate();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let span = document.querySelector(".date");
+span.innerHTML = `${day} ${date} ${hours}:${minutes}`;
+
+function showWeatherCondition(response) {
+  document.querySelector(".city").innerHTML = response.data.name;
+  document.querySelector(".temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+function searchCity(city) {
+  let apiKey = "765f905a5ff48de4791afc288658166a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeatherCondition);
+}
+
+let searchForm = document.querySelector("#searchForm");
+searchForm.addEventListener("submit", handleSubmit);
+
+searchCity("Lagos");
