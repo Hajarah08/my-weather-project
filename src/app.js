2569db482e373471f6f0c6a916c290ac1c2fa0d8
Hajarah08 <koikiadetutu@gmail.com>
2022-04-09 19:12:33 +0000

    update app.js

--- src/app.js
+++ src/app.js
@@ -14,11 +14,13 @@ if (minutes < 10) {
 return `${day}, ${hours}: ${minutes}` ;
 }
 
-function displayForecast(){
+function displayForecast(response){
+  let forecast = response.data
   let forecastElement = document.querySelector("#forecast"); 
   let forecastHTML =`<div class = "row">`;
   let days = ["Sat", "Sun", "Mon", "Tues", "Wed",];
-  days.forEach(function(day) {
+  days.forEach(function(forecastday) {
+
     forecastHTML =
       forecastHTML +
       `
@@ -34,6 +36,12 @@ function displayForecast(){
 
 forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey = "765f905a5ff48de4791afc288658166a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showWeatherCondition(response) {
@@ -51,6 +59,8 @@ function showWeatherCondition(response) {
     response.data.weather[0].description;
     document.querySelector(".date").innerHTML = formatDate(response.data.dt * 1000);
     document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+    getForecast(response.data.coord);
 }
 
 
@@ -94,5 +104,5 @@ fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
-displayForecast();
+
 searchCity("Lagos");
